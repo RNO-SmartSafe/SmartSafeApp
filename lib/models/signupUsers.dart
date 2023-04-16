23da8cffe364c92bbb42a8cd4ed9b1552917db72
Reut Hadad <reuthadad123@gmail.com>
2023-04-16 18:37:50 +0300

    add signup users model

---
+++ lib/models/signupUsers.dart
@@ -0,0 +1,15 @@
+class SignupUsers {
+  int id;
+  String name;
+  String email;
+  String password;
+  int phone;
+
+  SignupUsers({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.phone,
+  });
+}
