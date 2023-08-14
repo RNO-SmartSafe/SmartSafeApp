d32888f545d1698de9436275c9b32acbdb02a5c1
Reut Hadad <reuthadad123@gmail.com>
2023-08-14 22:54:37 +0300

    final update to report screen

--- lib/screens/reports/reports_screen.dart
+++ lib/screens/reports/reports_screen.dart
@@ -17,67 +17,72 @@ class _ReportsScreenState extends State<ReportsScreen> {
       appBar: AppBar(
         title: const Text('Reports'),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('reports').snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final data = snapshot.data!.docs;
-            data.sort((a, b) {
-              final timestampA = a['Time'] as Timestamp;
-              final timestampB = b['Time'] as Timestamp;
-              return timestampB.compareTo(timestampA);
-            });
-            return SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: DataTable(
-                columns: const [
-                  DataColumn(label: Text('Name')),
-                  DataColumn(label: Text('Harness ID')),
-                  DataColumn(label: Text('Safe')),
-                  DataColumn(label: Text('Time')),
-                  // Add more DataColumn widgets for additional columns
-                ],
-                rows: data.map((doc) {
-                  final column1Data = doc['Name'];
-                  final column2Data = doc['Harness ID'];
-                  final column3Data = doc['Safe'];
-                  final column4Data = doc['Time'] as Timestamp;
-                  final dateTime = column4Data.toDate();
-                  final formattedTime = dateTime.toString();
-                  // Add more variables for additional columns' data
+      body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection('Alerts').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final data = snapshot.data!.docs;
+              return SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: DataTable(
+                  columns: const [
+                    DataColumn(label: Text('Name')),
+                    //   DataColumn(label: Text('Harness ID')),
+                    // DataColumn(label: Text('Time')),
+                    DataColumn(label: Text('Date')),
+                    DataColumn(label: Text('IOT')),
+                    // DataColumn(label: Text('from')),
+                    // DataColumn(label: Text('message type')),
+                    // DataColumn(label: Text('messege number')),
+                  ],
+                  rows: data.map((doc) {
+                    final column5Data = doc['name'];
+                    // final column2Data = doc['Harness ID'];
+                    // final column3Data = doc['Safe'];
+                    // final column4Data = doc['Time'] as Timestamp;
+                    // final dateTime = column4Data.toDate();
+                    // final formattedTime = dateTime.toString();
 
-                  return DataRow(
-                    cells: [
-                      DataCell(Center(
-                          child: column3Data == 'Yes'
-                              ? const Icon(Icons.square, color: Colors.grey)
-                              : const Icon(Icons.rectangle,
-                                  color: Color.fromARGB(255, 153, 38, 38)))),
-                      DataCell(Center(child: Text(column1Data))),
-                      DataCell(Center(child: Text(column2Data))),
-                      DataCell(Center(child: Text(formattedTime))),
-                      // Add more DataCell widgets for additional columns' data
-                    ],
-                  );
-                }).toList(),
-              ),
-            );
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            return const CircularProgressIndicator();
-          }
-        },
+                    final column1Data = doc['Date'];
+                    final column2Data = doc['IOT'];
+                    // final column3Data = doc['from'];
+                    // final column4Data = doc['message type'];
+
+                    return DataRow(
+                      cells: [
+                        // DataCell(Center(child: Text(column1Data))),
+                        // DataCell(Center(child: Text(column2Data))),
+                        // DataCell(Center(child: Text(formattedTime))),
+
+                        DataCell(Center(child: Text(column5Data))),
+                        DataCell(Center(child: Text(column1Data))),
+                        DataCell(Center(child: Text(column2Data))),
+                        // DataCell(Center(child: Text(column3Data))),
+                        // DataCell(Center(child: Text(column4Data))),
+                      ],
+                    );
+                  }).toList(),
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              return const CircularProgressIndicator();
+            }
+          },
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final data = [
-            ['Name', 'Harness ID', 'Safe', 'Time'],
-            // Add your data rows here
-          ];
-        },
-        child: const Icon(Icons.file_download),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () async {
+      //     final data = [
+      //       // ['Name', 'Harness ID', 'Safe', 'Time'],
+      //       ['Safe', 'Date', 'IOT', 'from', 'message type']
+      //     ];
+      //   },
+      //   child: const Icon(Icons.file_download),
+      // ),
     );
   }
 }
